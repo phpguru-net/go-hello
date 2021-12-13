31aab689ed49115b4fd35f093c7bfa0cd13e2c7f
misostack <misostack.com@gmail.com>
2021-12-13 10:17:51 +0700

    Switch case

--- test/main.go
+++ test/main.go
@@ -90,6 +90,70 @@ func RemoveItem(slice []int, index int) []int {
 	return append(slice[:index], slice[index+1:]...)
 }
 
+// - Stand (S)
+// - Hit (H)
+// - Split (P)
+// - Automatically win (W)
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	var cardValues map[string]int = map[string]int{
+		"ace":   11,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"ten":   10,
+		"jack":  10,
+		"queen": 10,
+		"king":  10,
+		"other": 0,
+	}
+	value, ok := cardValues[card]
+	if ok {
+		return value
+	}
+	return 0
+}
+
+// IsBlackjack returns true if the player has a blackjack, false otherwise.
+// If 2 cards have the combined value of 21 ==> Blackjack
+func IsBlackjack(card1, card2 string) bool {
+	card1Value := ParseCard(card1)
+	card2Value := ParseCard(card2)
+	return card1Value+card2Value == 21
+}
+
+// LargeHand implements the decision tree for hand scores larger than 20 points.
+func LargeHand(isBlackjack bool, dealerScore int) string {
+	if !isBlackjack {
+		return "P"
+	}
+	if isBlackjack && dealerScore < 10 {
+		return "W"
+	}
+	return "S"
+}
+
+// SmallHand implements the decision tree for hand scores with less than 21 points.
+func SmallHand(handScore, dealerScore int) string {
+	switch {
+	case handScore >= 17:
+		return "S"
+	case handScore <= 11:
+		return "H"
+	case handScore >= 12 && handScore <= 16 && dealerScore <= 6:
+		return "S"
+	case handScore >= 12 && handScore <= 16 && dealerScore >= 7:
+		return "H"
+	}
+	return "S"
+}
+
 func main() {
 	fmt.Printf("%v need NeedsLicense? %v\n", "car", NeedsLicense("car"))
 	fmt.Printf("%v vs %v ? %v\n", "Bugatti Veyron", "Ford Pinto", ChooseVehicle("Bugatti Veyron", "Ford Pinto"))
@@ -113,4 +177,8 @@ func main() {
 	fmt.Println(newSlice)
 	slice = RemoveItem(slice, 3)
 	fmt.Println(slice)
+
+	fmt.Printf("%s = %v\n", "queen", ParseCard("queen"))
+	fmt.Printf("%s = %v\n", "ace", ParseCard("ace"))
+	fmt.Printf("%s = %v\n", "jack", ParseCard("jack"))
 }
